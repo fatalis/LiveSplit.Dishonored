3c2ea7e830e80bb5cb879a823dd73a7149be21dc
Ian Robinson <ianronrobinson@gmail.com>
2023-01-08 00:46:09 -0500

    add back settings

--- DishonoredSettings.cs
+++ DishonoredSettings.cs
@@ -14,21 +14,23 @@ namespace LiveSplit.Dishonored
         public bool AutoSplitOutsidersDream { get; set; }
         public bool AutoSplitWeepers { get; set; }
         public bool AutoSplitDLC06IntroEnd { get; set; }
+        public bool CutsceneSpeedup { get; set; }
 
         public DishonoredSettings()
         {
             InitializeComponent();
 
-            this.chkAutoStartEnd.DataBindings.Add("Checked", this, "AutoStartEnd", false, DataSourceUpdateMode.OnPropertyChanged);
-            this.chkAutoSplitIntroEnd.DataBindings.Add("Checked", this, "AutoSplitIntroEnd", false, DataSourceUpdateMode.OnPropertyChanged);
-            this.chkAutoSplitMissionEnd.DataBindings.Add("Checked", this, "AutoSplitMissionEnd", false, DataSourceUpdateMode.OnPropertyChanged);
-            this.chkAutoSplitPrisonEscape.DataBindings.Add("Checked", this, "AutoSplitPrisonEscape", false, DataSourceUpdateMode.OnPropertyChanged);
-            this.chkAutoSplitOutsidersDream.DataBindings.Add("Checked", this, "AutoSplitOutsidersDream", false, DataSourceUpdateMode.OnPropertyChanged);
-            this.chkAutoSplitWeepers.DataBindings.Add("Checked", this, "AutoSplitWeepers", false, DataSourceUpdateMode.OnPropertyChanged);
-            this.chkAutoSplitDLC06IntroEnd.DataBindings.Add("Checked", this, "AutoSplitDLC06IntroEnd", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkAutoStartEnd.DataBindings.Add("Checked", this, "AutoStartEnd", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkAutoSplitIntroEnd.DataBindings.Add("Checked", this, "AutoSplitIntroEnd", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkAutoSplitMissionEnd.DataBindings.Add("Checked", this, "AutoSplitMissionEnd", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkAutoSplitPrisonEscape.DataBindings.Add("Checked", this, "AutoSplitPrisonEscape", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkAutoSplitOutsidersDream.DataBindings.Add("Checked", this, "AutoSplitOutsidersDream", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkAutoSplitWeepers.DataBindings.Add("Checked", this, "AutoSplitWeepers", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkAutoSplitDLC06IntroEnd.DataBindings.Add("Checked", this, "AutoSplitDLC06IntroEnd", false, DataSourceUpdateMode.OnPropertyChanged);
+            chkCutsceneSpeedup.DataBindings.Add("Checked", this, "CutsceneSpeedup", false, DataSourceUpdateMode.OnPropertyChanged);
 
             // defaults
-            this.AutoStartEnd = true;
+            AutoStartEnd = true;
         }
 
         public XmlNode GetSettings(XmlDocument doc)
@@ -37,33 +39,34 @@ namespace LiveSplit.Dishonored
 
             settingsNode.AppendChild(ToElement(doc, "Version", Assembly.GetExecutingAssembly().GetName().Version.ToString(3)));
 
-            settingsNode.AppendChild(ToElement(doc, "AutoStartEnd", this.AutoStartEnd));
-            settingsNode.AppendChild(ToElement(doc, "AutoSplitIntroEnd", this.AutoSplitIntroEnd));
-            settingsNode.AppendChild(ToElement(doc, "AutoSplitMissionEnd", this.AutoSplitMissionEnd));
-            settingsNode.AppendChild(ToElement(doc, "AutoSplitPrisonEscape", this.AutoSplitPrisonEscape));
-            settingsNode.AppendChild(ToElement(doc, "AutoSplitOutsidersDream", this.AutoSplitOutsidersDream));
-            settingsNode.AppendChild(ToElement(doc, "AutoSplitWeepers", this.AutoSplitWeepers));
-            settingsNode.AppendChild(ToElement(doc, "AutoSplitDLC06IntroEnd", this.AutoSplitDLC06IntroEnd));
+            settingsNode.AppendChild(ToElement(doc, "AutoStartEnd", AutoStartEnd));
+            settingsNode.AppendChild(ToElement(doc, "AutoSplitIntroEnd", AutoSplitIntroEnd));
+            settingsNode.AppendChild(ToElement(doc, "AutoSplitMissionEnd", AutoSplitMissionEnd));
+            settingsNode.AppendChild(ToElement(doc, "AutoSplitPrisonEscape", AutoSplitPrisonEscape));
+            settingsNode.AppendChild(ToElement(doc, "AutoSplitOutsidersDream", AutoSplitOutsidersDream));
+            settingsNode.AppendChild(ToElement(doc, "AutoSplitWeepers", AutoSplitWeepers));
+            settingsNode.AppendChild(ToElement(doc, "AutoSplitDLC06IntroEnd", AutoSplitDLC06IntroEnd));
+            settingsNode.AppendChild(ToElement(doc, "CutsceneSpeedup", CutsceneSpeedup));
 
             return settingsNode;
         }
 
         public void SetSettings(XmlNode settings)
         {
-            this.AutoStartEnd = ParseBool(settings, "AutoStartEnd", true);
-            this.AutoSplitIntroEnd = ParseBool(settings, "AutoSplitIntroEnd");
-            this.AutoSplitMissionEnd = ParseBool(settings, "AutoSplitMissionEnd");
-            this.AutoSplitPrisonEscape = ParseBool(settings, "AutoSplitPrisonEscape");
-            this.AutoSplitOutsidersDream = ParseBool(settings, "AutoSplitOutsidersDream");
-            this.AutoSplitWeepers = ParseBool(settings, "AutoSplitWeepers");
-            this.AutoSplitDLC06IntroEnd = ParseBool(settings, "AutoSplitDLC06IntroEnd");
+            AutoStartEnd = ParseBool(settings, "AutoStartEnd", true);
+            AutoSplitIntroEnd = ParseBool(settings, "AutoSplitIntroEnd");
+            AutoSplitMissionEnd = ParseBool(settings, "AutoSplitMissionEnd");
+            AutoSplitPrisonEscape = ParseBool(settings, "AutoSplitPrisonEscape");
+            AutoSplitOutsidersDream = ParseBool(settings, "AutoSplitOutsidersDream");
+            AutoSplitWeepers = ParseBool(settings, "AutoSplitWeepers");
+            AutoSplitDLC06IntroEnd = ParseBool(settings, "AutoSplitDLC06IntroEnd");
+            CutsceneSpeedup = ParseBool(settings, "CutsceneSpeedup");
         }
 
         static bool ParseBool(XmlNode settings, string setting, bool default_ = false)
         {
-            bool val;
             return settings[setting] != null ?
-                (Boolean.TryParse(settings[setting].InnerText, out val) ? val : default_)
+                (Boolean.TryParse(settings[setting].InnerText, out var val) ? val : default_)
                 : default_;
         }
 
